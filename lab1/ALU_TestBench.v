10a9ce5678e908d6508fd4add193747b6408b0c0
Melvin Bosnjak <meco0597@gmail.com>
2018-09-05 15:54:38 -0600

    added the random testing in the testbench

--- lab1/ALU_TestBench.v
+++ lab1/ALU_TestBench.v
@@ -43,6 +43,14 @@ module ALU_TestBench;
 	// zero
 	Src = 16'd2; Dest = -16'd2;
 	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
 	
 	// add (unsigned)
 	Opcode = 8'b00000110;
@@ -55,41 +63,76 @@ module ALU_TestBench;
 	// carry
 	Src = 16'b1111111111111111; Dest = 1;
 	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
 	
 	// subtract
 	Opcode = 8'b00001001;
-	#10;
 	// zero
 	Src = 16'd3; Dest = 16'd3;
 	#10;	
 	// overflow
 	Src = 16'b1111111111111110; Dest = 16'b0111111111111111;	
 	#10;
-	
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
+
 	//and
 	Opcode = 8'b00000001;
-	#10;
 	// zero
 	Src = 16'b0; Dest = 16'b1;	
 	#10;
-	
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
+
 	//or
 	Opcode = 8'b00000010;
-	#10;
 	// zero
 	Src = 16'b0; Dest = 16'b0;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
 	#10;
 	
 	//xor
 	Opcode = 8'b00000011;
-	#10;
 	// zero
 	Src = 16'b0; Dest = 16'b0;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
 	#10;
 	
 	// compare
 	Opcode = 8'b00001011;
-	#10;
 	//negative = 1
 	Src = 16'b1; Dest = 16'b0;
 	#10;
@@ -99,38 +142,47 @@ module ALU_TestBench;
 	// negative = 0
 	Src = 16'b1111111111111110; Dest = 16'b1111111111111111;
 	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
 	
 	// LHS
 	Opcode = 8'b10000100;
-	#10;
 	Dest = 16'b1;
 	#10;
 	// Should this result in zero?
 	Dest = 16'b1000000000000000;
 	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
 	
 	// RHS
 	Opcode = 8'b10001100;
-	#10;
 	Dest = 16'b1;
 	#10;
 	Dest = 16'b1000000000000000;
 	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		Src = $random % 16;
+		Dest = $random % 16;
+	end
+	#10;
 	
-	
-	
-
-	
-//	//Random simulation
-//	for( i = 0; i< 10; i = i+ 1)
-//	begin
-//		#10
-//		A = $random % 16;
-//		B = $random %16;
-//		$display("A: %0d, B: %0d, C: %0d, Flags[1:0]: %b, time:%0d", A, B, C, Flags[1:0], $time );
-//	end
-//	$finish(2);
-		
-    
+	$finish(2);
+		   
 	 end  
 endmodule
