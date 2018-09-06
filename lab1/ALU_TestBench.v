4130b46bfd56e0d456f222af6be4c3269bf902bf
Kyle Price <gc.price4@gmail.com>
2018-09-05 19:02:03 -0600

    fix bench copy error

--- lab1/ALU_TestBench.v
+++ lab1/ALU_TestBench.v
@@ -1,201 +1,190 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: Superfriends team
-// Engineer: Jaden, Dan, Kyle, Melvin
-// 
-// Create Date:    08/30/2018 
-// Design Name:    ALU
-// Module Name:    ALU 
-// Project Name:   Lab 1
-//////////////////////////////////////////////////////////////////////////////////
-module ALU( DST, SRC, C, c_in,Opcode, Flags);
-input [15:0] DST, SRC;
-input [7:0] Opcode;
-input c_in;
+module ALU_TestBench;
 
-output reg [15:0] C;
-output reg [4:0] Flags;
-// Flag [4] = Z (Zero)
-// Flag [3] = C (Carry)
-// Flag [2] = O (Overflow) Note: This flag may sometimes be called 'F' in some documents
-// Flag [1] = L (Low)
-// Flag [0] = N (Negative)
+	// Inputs
+	reg [7:0] Opcode;
+	reg [15:0] SRC;
+	reg [15:0] DST;	
+	
+	// Outputs
+	wire [15:0] C;
+	wire [4:0] Flags;
+	
+	// counter variable
+	integer i;
+	
+	// Instantiate the DE1 Soc(de1soc)
+	ALU test1(
+		.SRC(SRC), 
+		.DST(DST),
+		.Opcode(Opcode),
+		.c_in(c_in),
+		.Flags(Flags),
+		.C(C)
+	);
+	
+	initial begin
+	
+	// Initialize Inputs
+	Opcode = 0;
+	SRC = 0;
+	DST = 0;
+	c_in = 0;
+	
+	$monitor("SRC: %0d, DST: %0d, C: %0d, Flags[4:0]: %b, time:%0d", SRC, DST, C, Flags[4:0], $time );
 
-// Currently the ALU design can handle 3 groups of opcodes: Special, Register, and Shift
-// Eventually, it may be better to have Special and Shift instructions handled outside of the ALU.
-// Special instructions could be handled by the CPU control unit as they are relatively simple.
-// There aren't many Shift instructions so they could just be handled by a shifter module.
+	// [4] = z, [2] = overflow, 
+	
+	#10;
+	
+	// add (signed)
+	Opcode = 8'b00000101; 
+	// overflow
+	SRC = 16'b0111111111111111; DST = 16'b0111111111111111;
+	#10;
+	// zero
+	SRC = 16'd2; DST = -16'd2;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	// add (unsigned)
+	Opcode = 8'b00000110;
+	// carry
+	SRC = 16'b1111111111111111; DST = 1;
+	#10;
+	// zero
+	SRC = 0; DST = 0;
+	#10;
+	// carry
+	SRC = 16'b1111111111111111; DST = 1;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	// subtract
+	Opcode = 8'b00001001;
+	// zero
+	SRC = 16'd3; DST = 16'd3;
+	#10;	
+	// overflow
+	SRC = 16'b1111111111111110; DST = 16'b0111111111111111;	
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
 
-// Immediate Instructions:
-// A CPU controller can also handle immediate instructions pretty easily, no reason to add a bunch of 
-// boiler plate code when there's a much more elegant solution. If you look at the chart of instructions
-// and their corresponding opcodes, you'll notice that the high 4-bits of the opcode for immediate instructions
-// are the exact same as the lower 4-bits of the non-immediate version. The control unit would simply route the
-// higher 4-bits to the ALU and set a flag for a mux to take in the immediate value at SRC.
+	//and
+	Opcode = 8'b00000001;
+	// zero
+	SRC = 16'b0; DST = 16'b1;	
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
 
-
-// Special group
-parameter LOAD = 4'b0000;
-
-// Register group
-parameter AND  = 4'b0001;
-parameter OR   = 4'b0010;
-parameter XOR  = 4'b0011;
-parameter ADD  = 4'b0101;
-parameter ADDU = 4'b0110;
-parameter ADDC = 4'b0111;
-parameter SUB  = 4'b1001;
-parameter CMP  = 4'b1011;
-parameter MOV  = 4'b1101;
-
-// Shift group
-parameter LHS 	= 4'b0100;
-parameter RHS	= 4'b1100;
-
-// Upper 4 bits of the opcode define groups
-parameter Register = 4'b0000;
-parameter Shift = 	4'b1000;
-parameter Special = 	4'b0100;
-
-// Always block for combinational logic
-always @(SRC, DST, Opcode, c_in)
-begin
-	case (Opcode[7:4]) // First case statement is for the upper 4-bits
-	Register:
-		begin
-		case(Opcode[3:0]) // Check the lower 4-bits in the Register group
-		AND: // Logical operations are simple; only need to check for Z flag
-			begin
-			C = SRC & DST;
-			Flags[3:0] = 4'b0000;
-			Flags[4] = C == 0;
-			end
-		OR:
-			begin
-			C = SRC | DST;
-			Flags[3:0] = 4'b0000;
-			Flags[4] = C == 0;		
-			end
-		XOR:
-			begin
-			C = SRC ^ DST;
-			Flags[3:0] = 4'b0000;
-			Flags[4] = C == 0;
-			end
-		ADD: // Signed addition
-			begin
-			C = SRC + DST;
-			if (C == 0) Flags[4] = 1'b1;
-			else Flags[4] = 1'b0;
-			if( (~SRC[15] & ~DST[15] & C[15]) | (SRC[15] & DST[15] & ~C[15]) ) Flags[2] = 1'b1; // Check for signed overflow here
-			else Flags[2] = 1'b0;
-			Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-			end
-		ADDU:
-			begin
-			{Flags[3], C} = SRC + DST; // Set the carry flag
-			if (C == 0) Flags[4] = 1'b1; 
-			else Flags[4] = 1'b0;
-			Flags[2:0] = 3'b000;
-			end
-		ADDC: // Signed addition with a carry in
-			begin
-			C = SRC + DST + c_in; // Same as ADD but use c_in
-			if (C == 0) Flags[4] = 1'b1;
-			else Flags[4] = 1'b0;
-			if( (~SRC[15] & ~DST[15] & C[15]) | (SRC[15] & DST[15] & ~C[15]) ) Flags[2] = 1'b1; // Check for signed overflow here
-			else Flags[2] = 1'b0;
-			Flags[2:0] = 2'b00; Flags[3] = 1'b0;
-			end
-		SUB: // Signed subtraction, could add unsigned subtraction later, though it doesn't make much sense
-			begin
-			C = SRC - DST;
-			if (C == 0) Flags[4] = 1'b1;
-			else Flags[4] = 1'b0;
-			if( (~SRC[15] & ~DST[15] & C[15]) | (SRC[15] & DST[15] & ~C[15]) ) Flags[2] = 1'b1;
-			else Flags[2] = 1'b0;
-			Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-			end
-		CMP: // Signed comparison
-			begin
-			if( $signed(SRC) < $signed(DST) ) Flags[1:0] = 2'b11;
-			else Flags[1:0] = 2'b00;
-			C = 0;
-			Flags[4:2] = 3'b000;
-			end
-		MOV: // Sets all flags to 0 and C = SRC
-			begin
-				C = SRC;
-				Flags[4:0] = 5'b00000;
-			end
-		default:
-			begin
-			C = 0;
-			Flags = 5'b0000;
-			end
-		endcase
-		end
-	Shift: // Shift group, may be moved into a separate module eventually
-		begin
-		case(Opcode[3:0])
-		LHS: // Currently only does 1 bit signed shifts
-			begin
-			C = $signed(DST) << 1;
-			Flags = 5'b00000;
-			end
-		/*LHSI:
-			begin
-			C = $signed(DST) << 1;
-			Flags = 5'b00000;
-			end
-		LHSIS:
-			begin
-			C = $signed(DST) << 1;
-			Flags = 5'b00000;
-			end*/
-		RHS:
-			begin
-			C = $signed(DST) >> 1;
-			Flags = 5'b00000;
-			end
-		/*RHSI:
-			begin
-			C = $signed(DST) >> 1;
-			Flags = 5'b00000;
-			end
-		RHSIS:
-			begin
-			C = $signed(DST) >> 1;
-			Flags = 5'b00000;
-			end*/
-		default:
-			begin
-				C=0;
-				Flags = 5'b00000;
-			end
-		endcase
-		end
-	Special:
-		begin
-		case(Opcode[3:0])
-			LOAD:
-			begin
-				C = SRC;
-				Flags = 5'b00000;
-			end
-		default:
-			begin
-				C=0;
-				Flags = 5'b00000;
-			end
-		endcase
-		end
-	default: 
-		begin
-			C = 0;
-			Flags = 5'b00000;
-		end
-	endcase
-end
-
+	//or
+	Opcode = 8'b00000010;
+	// zero
+	SRC = 16'b0; DST = 16'b0;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	//xor
+	Opcode = 8'b00000011;
+	// zero
+	SRC = 16'b0; DST = 16'b0;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	// compare
+	Opcode = 8'b00001011;
+	//negative = 1
+	SRC = 16'b1; DST = 16'b0;
+	#10;
+	//negative = 0
+	SRC = 16'b0; DST = 16'b1;
+	#10;
+	// negative = 0
+	SRC = 16'b1111111111111110; DST = 16'b1111111111111111;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	// LHS
+	Opcode = 8'b10000100;
+	DST = 16'b1;
+	#10;
+	// Should this result in zero?
+	DST = 16'b1000000000000000;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	// RHS
+	Opcode = 8'b10001100;
+	DST = 16'b1;
+	#10;
+	DST = 16'b1000000000000000;
+	#10;
+	// Random
+	for( i = 0; i< 10; i = i+ 1)
+	begin
+		#10
+		SRC = $random % 16;
+		DST = $random % 16;
+	end
+	#10;
+	
+	$finish(2);
+		   
+	 end  
 endmodule
