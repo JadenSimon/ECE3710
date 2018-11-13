097161ad0ba42bc2b2777eeab12b2d81de127794
Melvin Bosnjak <meco0597@gmail.com>
2018-11-13 11:06:53 -0700

    added the assembly pseudocode and the conventions file

---
+++ conventions.v
@@ -0,0 +1,10 @@
+// Memory Addresses for player orientation and location
+loc1x =
+
+// Register conventions
+REG0 - REG4 // temporary register
+REG5 - REG10 // saved register
+REG11 // Return register
+REG13 // Jump target register
+REG14 // arg1
+REG15 // arg2
