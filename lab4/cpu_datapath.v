0b789cc5424810926073c52b281e47bc1fbaa711
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2018-11-15 08:39:30 -0700

    Started SNES instruction

--- lab4/cpu_datapath.v
+++ lab4/cpu_datapath.v
@@ -18,7 +18,7 @@ module cpu_datapath(clk, reset, data_out);
 	// Set up bram wires
 	wire [15:0] data_a, data_b;
 	wire [15:0] addr_b;
-	wire we_a, we_b;
+	wire we_a, we_b, fsm_we;
 	wire [15:0] q_a, q_b;
 	
 	// Set up ALU wires
@@ -37,25 +37,24 @@ module cpu_datapath(clk, reset, data_out);
 	wire ld_mux, pc_mux, jal_mux, branch_mux;
 	
 	// Set up PC wires
-	wire PCEnable, pc_load;
+	wire PCEnable, pc_load, fsm_pc;
 	wire [15:0] PCOut;
-	wire [15:0] PCAdder;
 	
 	// Set up mux output wires
 	wire [15:0] LoadMuxOut;
 	wire [15:0] PCMuxOut, PCMuxIn;
 	
 	// Generate muxes and assign values to wires
-	assign DST = RegWire[mux_A];
+	assign DST = branch_mux ? PCOut : RegWire[mux_A];
 	assign SRC = RegWire[mux_B];
-	assign PCMuxOut = pc_mux ? (branch_mux ? (PCMuxIn - 16'b1) : ALUOutput) : PCOut;
+	assign PCMuxOut = pc_mux ? ALUOutput : PCOut;
 	assign LoadMuxOut = jal_mux ? PCOut : (ld_mux ? q_a : ALUOutput);
-	// Immediate must be sign extended for branching
-	assign PCAdder = branch_mux ? {{8{Immediate[7]}}, Immediate[7:0]} : 16'b1;
-	assign PCMuxIn = (pc_load ? ALUOutput : PCOut) + PCAdder;
+	assign PCMuxIn = (pc_load ? ALUOutput : PCOut) + 1'b1;
 
 	// Connect DST to data_a
 	assign data_a = DST;
+	assign we_a = reset ? 1'b0 : fsm_we;
+	assign PCEnable = reset ? 1'b0 : fsm_pc;
 	
 	// Assign dummy values for unhooked up components (will be used by VGA later)
 	assign addr_b = 16'b0;
@@ -69,7 +68,7 @@ module cpu_datapath(clk, reset, data_out);
 	flags FlagsRegUnit(clk, reset, FlagsEnable, ALUFlags, RegFlags);
 	ALU ALUUnit(DST, SRC, Immediate, ALUOutput, RegFlags[3], Opcode, ALUFlags);
 	Bram BlockRam(data_a, data_b, PCMuxOut, addr_b, we_a, we_b, clk, q_a, q_b);
-	control_fsm FSMUnit(clk, reset, q_a, RegFlags, ld_mux, pc_mux, jal_mux, pc_load, branch_mux, mux_A, mux_B, Opcode, RegEnable, FlagsEnable, PCEnable, we_a, Immediate);
+	control_fsm FSMUnit(clk, reset, q_a, RegFlags, ld_mux, pc_mux, jal_mux, pc_load, branch_mux, mux_A, mux_B, Opcode, RegEnable, FlagsEnable, fsm_pc, fsm_we, Immediate);
 	program_counter PC(clk, reset, PCEnable, PCMuxIn, PCOut);
 	RegFile RegFileUnit(LoadMuxOut, RegWire[0], RegWire[1], RegWire[2], RegWire[3],
 										RegWire[4], RegWire[5], RegWire[6], RegWire[7],
