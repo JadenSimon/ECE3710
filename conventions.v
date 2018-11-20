b185ecf82c5996570adff572a62982706898701b
Melvin Bosnjak <meco0597@gmail.com>
2018-11-20 14:28:06 -0700

    Fixed somethings with the orientation and location of players.

--- conventions.v
+++ conventions.v
@@ -1,33 +1,37 @@
-// Memory Addresses for player orientation and location Orientation is the upper two bits and location is the lower 14 bit
-loc1x = 0000 1111 0000 1111 // Location for player one x coordinate
-loc1y = 0000 1111 0000 1110 // Location for player one y coordinate
-loc2x = 0000 1111 0000 1101 // Location for player one x coordinate
-loc2y = 0000 1111 0000 1100 // Location for player one y coordinate
+// Memory Addresses for player location
+player 1 x-coordinate = 0000 1111 0000 1111
+player 1 y-coordinate = 0000 1111 0000 1110
+player 2 x-coordinate = 0000 1111 0000 1101
+player 2 y-coordinate = 0000 1111 0000 1100
 
-// Orientation: 00 = UP, 01 = RIGHT, 10 = DOWN, 11 = LEFT 
+// Player health addresses
+player 1 health = 0000 1110 0111 0100
+player 2 health = 0000 1110 0111 0011
 
-// Memory addresses for projectiles 15 is the valid bit, 13-14 is orientation/direction and 0-12 is position
-proj1x = 0000 1111 0000 1011 // Location for projectile one x coordinate
-proj1y = 0000 1111 0000 1010 // Location for projectile one y coordinate
-proj2x = 0000 1111 0000 1001 // Location for projectile two x coordinate
-proj2y = 0000 1111 0000 1000 // Location for projectile two y coordinate
+// Orientation addresses. Orientation: 00 = UP, 01 = RIGHT, 10 = DOWN, 11 = LEFT
+player 1 orientation = 0000 1110 0111 0010
+player 2 orientation = 0000 1110 0111 0001
+projectile 1 orientation = 0000 1110 0111 0000
+projectile 2 orientation = 0000 1110 0110 1111
+
+// Memory addresses for projectiles 15 is the valid bit and 0-14 is position
+projectile 1 x-coordinate = 0000 1111 0000 1011
+projectile 1 y-coordinate = 0000 1111 0000 1010
+projectile 2 x-coordinate = 0000 1111 0000 1001
+projectile 2 y-coordinate = 0000 1111 0000 1000
 
 // Game state address, will tell the vga fsm whether to draw the start screen or the game screen
 // if the value at this address is 0 then draw the start menu if its anything else draw the game screen
-game_state = 0000 1111 0000 0111
-
-// Health of players address
-health1 = 0000 1111 0000 0110
-health2 = 0000 1111 0000 0101
+game state = 0000 1111 0000 0111
 
 // Stack region of memory
-top_of_stack = 0000 1111 0000 0100
-bottom_of_stack = 0000 1110 1111 0000
+top of stack = 0000 1111 0000 0100
+bottom of stack = 0000 1110 1111 0000
 
 // this will hold the coordinates of the walls in the map so we can do collision detection
 // since the coordinates don't need to be too big we will pack x coord and y coord in 16 bits
-end_of_wall_coordinates = 0000 1110 1110 1111
-start_of_wall_coordinates = 0000 1110 1101 1100
+end of wall coordinates = 0000 1110 1110 1111
+start of wall coordinates = 0000 1110 1101 1100
 
 // Register conventions
 REG0 - REG4 // temporary register
