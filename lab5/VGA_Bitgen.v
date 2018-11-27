d48af534922fe5cdc48124811c0eba50a436e5fb
danhumeniuk <35534588+danhumeniuk@users.noreply.github.com>
2018-11-27 07:31:50 -0700

    Checked logic on VGA_Display, added to bitgen

--- lab5/VGA_Bitgen.v
+++ lab5/VGA_Bitgen.v
@@ -7,8 +7,27 @@ input [7:0] pixelData;
 input [9:0] hCount;
 input [9:0] vCount;
 input [9:0] xLocation;
-input [9:0] yLocation;
+input [8:0] yLocation;
 
 output reg [7:0] rgb;
+
+parameter BLACK = 8’b000_000_00;
+parameter WHITE = 8’b111_111_11;
+parameter RED = 8’b111_000_00;
+parameter GREEN = 8’b000_111_00;
+parameter BLUE = 8’b000_000_00;
+
+always@(*)
+begin
+	if(~bright)
+		rgb = BLACK;
+	else
+	begin
+		if()
+		else if()
+		else
+			rgb = RED;
+	end
+end
 
 endmodule 
--- lab5/VGA_Display.v
+++ lab5/VGA_Display.v
@@ -34,18 +34,18 @@ begin
 		if (h_count == LINE) // end of line
 		begin
 			h_count <= 0;
-			hSync <= (h_count >= HS_STA && h_count <= HS_END);
+			hSync <= ~(h_count >= HS_STA && h_count <= HS_END);
 		end
 		else	// keeps on down the line
 		begin
 			h_count <= h_count + 1;
-			hSync <= (h_count >= HS_STA && h_count <= HS_END);
+			hSync <= ~(h_count >= HS_STA && h_count <= HS_END);
 		end
 	end
 	else
 	begin
 		h_count <= h_count;
-		hSync <= (h_count >= HS_STA && h_count <= HS_END);
+		hSync <= ~(h_count >= HS_STA && h_count <= HS_END);
 	end
 end
 
@@ -63,31 +63,31 @@ begin
 			if(v_count == SCREEN) //At bottom of screen
 			begin
 				v_count <= 10'b0;
-				vSync <= (v_count >= VS_STA && v_count <= VS_END);
+				vSync <= ~(v_count >= VS_STA && v_count <= VS_END);
 			end
 			else if(h_count == LINE) //New line
 			begin
 				v_count <= v_count + 1;
-				vSync <= (v_count >= VS_STA && v_count <= VS_END);
+				vSync <= ~(v_count >= VS_STA && v_count <= VS_END);
 			end
 			else //No new line
 			begin
 				v_count <= v_count;
-				vSync <= v_count >= VS_STA && v_count <= VS_END;
+				vSync <= ~(v_count >= VS_STA && v_count <= VS_END);
 			end
 
 		end
 		else
 		begin
 			v_count <= v_count;
-			vSync <= (v_count >= VS_STA && v_count <= VS_END);
+			vSync <= ~(v_count >= VS_STA && v_count <= VS_END);
 		end
 end
 
 //Sets bright signal when h_count is in the range and v_count is in range
 always@(posedge clock)
 begin
-	bright <= (h_count >= HA_STA && v_count >= VA_STA);
+	bright <= (h_count >= HA_STA && v_count <= VA_END);
 end
 
 always@(posedge clock)
