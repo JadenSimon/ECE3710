3a6c512e5de263fe58ec87bf9b9e5c3f951f83f5
Kyle Price <gc.price4@gmail.com>
2018-12-12 16:35:40 -0700

    final

--- SNES/SNES_Controller.v
+++ SNES/SNES_Controller.v
@@ -4,7 +4,7 @@
 // Engineer: Jaden, Dan, Kyle, Melvin
 //
 // Create Date:    11/06/2018
-// Design Name:    SNES_Controller
+// Design Name:    SNES_Controller 
 // Module Name:    SNES_Controller
 // Project Name:   SNES
 //////////////////////////////////////////////////////////////////////////////////
@@ -13,25 +13,25 @@
 // Modified to use an additional register to act as a buffer for user input.
 // In this way we can be sure that the output of the module is always valid.
 // Input clock should be a slow clock
-module SNES_Controller(clk, data_in, latch, data_out);
+module SNES_Controller(clk, data_in, latch, data_out); 
 
 	// Global wires
-	input wire clk, data_in;
+	input wire clk, data_in; 
 
 	// state variable
 	reg [1:0] state = 2'b00;
-	
-	// data counter and slow clock counter
-	reg [3:0] counter = 4'b0000;
+	 
+	// data counter and slow clock counter  
+	reg [3:0] counter = 4'b0000; 
 	reg [15:0] temp_data = 16'b0;
 
 	// 4 states
 	parameter start = 2'b00, latch_set = 2'b01, reading = 2'b10, done_reading = 2'b11;
-
+ 
 	// output registers
 	output reg latch;
 	output reg [15:0] data_out = 16'b0;
-	
+	 
 	// capture serial data on serial line on negedge clk
 	always@(negedge clk)
 	begin
